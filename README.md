70856fc8e3427cb4c661bec22a96eb536a167482
pvanliefland <pierre.vanliefland@gmail.com>
2024-11-29 22:00:06 +0100

    feat: finish setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# AoC 2024
+
+My repo for the Advent of Code 2024, in Rust 🦀
+
--- day00/src/main.rs
+++ day00/src/main.rs
@@ -1,3 +1,17 @@
+const INPUT_TEST: &str = include_str!("../input_test.txt");
+const INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    println!("Part 1   test          {} ", part_1(INPUT_TEST));
+    println!("         validation    {} ", part_1(INPUT));
+    println!("Part 2   test          {} ", part_2(INPUT_TEST));
+    println!("         validation    {} ", part_2(INPUT_TEST));
+}
+
+fn part_1(input: &str) -> u32 {
+    input.trim().parse::<u32>().unwrap()
+}
+
+fn part_2(input: &str) -> u32 {
+    input.trim().parse::<u32>().unwrap()
 }
