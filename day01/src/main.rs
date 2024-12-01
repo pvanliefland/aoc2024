7c78adc27c32b2ff0e9538d18769c0782ddbb0f1
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-01 07:29:35 +0100

    chore: cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,43 +4,32 @@ const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    let (mut test_left, mut test_right) = left_and_right(INPUT_TEST);
-    let (mut validation_left, mut validation_right) = left_and_right(INPUT);
-    println!(
-        "Part 1   test          {} ",
-        part_1(&mut test_left, &mut validation_right)
-    );
-    println!(
-        "         validation    {} ",
-        part_1(&mut validation_left, &mut validation_right)
-    );
-    println!(
-        "Part 2   test          {} ",
-        part_2(&mut test_left, &mut test_right)
-    );
-    println!(
-        "         validation    {} ",
-        part_2(&mut validation_left, &mut validation_right)
-    );
+    let mut test_input = parse(INPUT_TEST);
+    let mut input = parse(INPUT);
+    println!("Part 1   test          {} ", part_1(&mut test_input));
+    println!("         validation    {} ", part_1(&mut input));
+    println!("Part 2   test          {} ", part_2(&mut test_input));
+    println!("         validation    {} ", part_2(&mut input));
 }
 
-fn part_1(left: &mut [u32], right: &mut [u32]) -> u32 {
-    left.sort();
-    right.sort();
-    zip(left, right)
+fn part_1(input: &mut (Vec<u32>, Vec<u32>)) -> u32 {
+    input.0.sort();
+    input.1.sort();
+    zip(&input.0, &input.1)
         .map(|(left, right)| left.abs_diff(*right))
         .sum()
 }
 
-fn part_2(left: &mut [u32], right: &mut [u32]) -> u32 {
-    left.iter()
-        .map(|left| left * right.iter().filter(|&right| left == right).count() as u32)
+fn part_2(input: &mut (Vec<u32>, Vec<u32>)) -> u32 {
+    input
+        .0
+        .iter()
+        .map(|left| left * input.1.iter().filter(|&right| left == right).count() as u32)
         .sum()
 }
 
-fn left_and_right(input: &str) -> (Vec<u32>, Vec<u32>) {
-    let mut left_list = vec![];
-    let mut right_list = vec![];
+fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let (mut left_list, mut right_list) = (vec![], vec![]);
     input.trim().lines().for_each(|line| {
         let (left, right) = line.split_once("   ").unwrap();
         left_list.push(left.parse::<u32>().unwrap());
