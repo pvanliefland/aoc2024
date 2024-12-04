af69df8fa8d2470f1f62d061c98349d8fc2c709b
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-04 19:22:48 +0100

    feat: day03 working

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -8,137 +8,109 @@ fn main() {
     let test_input_1 = parse(INPUT_TEST_1);
     let test_input_2 = parse(INPUT_TEST_2);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(test_input_1));
-    // println!("         validation    {} ", part_1(input));
-    // println!("Part 2   test          {} ", part_2(test_input_2));
-    // println!("         validation    {} ", part_2(input));
+    println!("Part 1   test          {} ", part_1(&test_input_1));
+    println!("         validation    {} ", part_1(&input));
+    println!("Part 2   test          {} ", part_2(&test_input_2));
+    println!("         validation    {} ", part_2(&input));
 }
 
 enum State {
-    Unknown,
-    DoOrDont(char),
     Do,
-    Dont(char),
+    Dont,
     Mul(Option<String>, Option<String>, bool),
 }
 
-fn part_1(input: Input) -> u32 {
+fn part_1(input: &Input) -> u32 {
     process(input, false)
 }
 
-fn part_2(input: Input) -> u32 {
+fn part_2(input: &Input) -> u32 {
     process(input, true)
 }
 
-fn process(input: Input, dos_and_donts: bool) -> u32 {
-    input
-        .iter()
-        .map(|line| {
-            let mut valid = 0u32;
-            let mut state = State::Unknown;
-
-            let mut cursor = 0;
-
-            loop {
-                // smallest valid thing is mul(1,1) so 8 chars
-                if cursor > line.len() - 8 {
-                    break;
-                }
-
-                match state {
-                    State::Unknown | State::Do => {
-                        if &line[cursor..cursor + 4] == "mul(" {
-                            state = State::Mul(None, None, false);
-                            cursor += 4;
-                        } else {
-                            cursor += 1;
-                        }
-                    }
-                    State::DoOrDont(_) => todo!(),
-                    State::Dont(_) => todo!(),
-                    State::Mul(None, None, false) => {
-                        if line[cursor..cursor + 1]
-                            .chars()
-                            .next()
-                            .unwrap()
-                            .is_ascii_digit()
-                        {
-                            state =
-                                State::Mul(Some(line[cursor..cursor + 1].to_string()), None, false);
-                            cursor += 1;
-                        } else {
-                            state = State::Unknown;
-                            cursor += 1;
-                        }
-                    }
-                    State::Mul(Some(left), None, false) => {
-                        if line[cursor..cursor + 1]
-                            .chars()
-                            .next()
-                            .unwrap()
-                            .is_ascii_digit()
-                        {
-                            state = State::Mul(Some(left + &line[cursor..cursor + 1]), None, false);
-                            cursor += 1;
-                        } else if &line[cursor..cursor + 1] == "," {
-                            state = State::Mul(Some(left), None, true);
-                            cursor += 1;
-                        } else {
-                            state = State::Unknown;
-                            cursor += 1;
-                        }
-                    }
-                    State::Mul(Some(left), None, true) => {
-                        if line[cursor..cursor + 1]
-                            .chars()
-                            .next()
-                            .unwrap()
-                            .is_ascii_digit()
-                        {
-                            state = State::Mul(
-                                Some(left),
-                                Some(line[cursor..cursor + 1].to_string()),
-                                true,
-                            );
-                            cursor += 1;
-                        } else {
-                            state = State::Unknown;
-                            cursor += 1;
-                        }
-                    }
-                    State::Mul(Some(left), Some(right), true) => {
-                        if line[cursor..cursor + 1]
-                            .chars()
-                            .next()
-                            .unwrap()
-                            .is_ascii_digit()
-                        {
-                            state = State::Mul(
-                                Some(left),
-                                Some(right + &line[cursor..cursor + 1]),
-                                true,
-                            );
-                            cursor += 1;
-                        } else if &line[cursor..cursor + 1] == ")" {
-                            dbg!("r");
-                            valid += left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap();
-                            state = State::Unknown;
-                            cursor += 1;
-                        } else {
-                            state = State::Unknown;
-                            cursor += 1;
-                        }
-                    }
-                    _ => {
-                        state = State::Unknown;
+fn process(input: &Input, dos_and_donts: bool) -> u32 {
+    let mut state = State::Do;
+    let mut valid = 0u32;
+    for line in input {
+        let mut cursor = 0;
+        loop {
+            if cursor >= line.len() {
+                break;
+            } else if let Some("don't()") = line.get(cursor..cursor + 7) {
+                if dos_and_donts {
+                    state = State::Dont;
+                };
+                cursor += 7;
+            }
+            match state {
+                State::Do => {
+                    if let Some("mul(") = line.get(cursor..cursor + 4) {
+                        state = State::Mul(None, None, false);
+                        cursor += 4;
+                    } else {
                         cursor += 1;
                     }
                 }
+                State::Dont => {
+                    if let Some("do()") = line.get(cursor..cursor + 4) {
+                        state = State::Do;
+                        cursor += 4;
+                    } else {
+                        cursor += 1;
+                    }
+                }
+                State::Mul(None, None, false) => {
+                    if let Some(digit) = get_digit(line, cursor) {
+                        state = State::Mul(Some(digit.to_string()), None, false);
+                        cursor += 1;
+                    } else {
+                        state = State::Do;
+                        cursor += 1;
+                    }
+                }
+                State::Mul(Some(left), None, false) => {
+                    if let Some(digit) = get_digit(line, cursor) {
+                        state = State::Mul(Some(left + &digit.to_string()), None, false);
+                    } else if &line[cursor..cursor + 1] == "," {
+                        state = State::Mul(Some(left), None, true);
+                    } else {
+                        state = State::Do;
+                    }
+                    cursor += 1;
+                }
+                State::Mul(Some(left), None, true) => {
+                    if let Some(digit) = get_digit(line, cursor) {
+                        state = State::Mul(Some(left), Some(digit.to_string()), true);
+                    } else {
+                        state = State::Do;
+                    }
+                    cursor += 1;
+                }
+                State::Mul(Some(left), Some(right), true) => {
+                    if let Some(digit) = get_digit(line, cursor) {
+                        state = State::Mul(Some(left), Some(right + &digit.to_string()), true);
+                    } else if &line[cursor..cursor + 1] == ")" {
+                        valid += left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap();
+                        state = State::Do;
+                    } else {
+                        state = State::Do;
+                    }
+                    cursor += 1;
+                }
+                _ => {
+                    state = State::Do;
+                    cursor += 1;
+                }
             }
+        }
+    }
+    valid
+}
 
-            valid
-        })
-        .sum()
+fn get_digit(line: &str, position: usize) -> Option<char> {
+    line.get(position..position + 1)
+        .map(|slice| slice.chars().next().unwrap())
+        .filter(|char| char.is_ascii_digit())
 }
 
 fn parse(input: &str) -> Input {
