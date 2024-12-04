ffd04e37213de9d31be92fbe6f23485a20b08c6e
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-04 07:03:51 +0100

    feat: day04 part 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,48 +1,46 @@
 const INPUT_TEST: &str = include_str!("../input_test.txt");
-// const INPUT: &str = include_str!("../input.txt");
+const INPUT: &str = include_str!("../input.txt");
 
 type Input = Vec<Vec<char>>;
 
 fn main() {
     let test_input = parse(INPUT_TEST);
-    // let input = parse(INPUT);
+    let input = parse(INPUT);
     println!("Part 1   test          {} ", part_1(test_input));
-    // println!("         validation    {} ", part_1(input));
+    println!("         validation    {} ", part_1(input));
     // println!("Part 2   test          {} ", part_2(test_input));
     // println!("         validation    {} ", part_2(input));
 }
 
 fn part_1(input: Input) -> u32 {
     let mut total = 0;
-    let x_pos = input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(move |(x, char)| match char {
-                    'X' => Some((x, y)),
-                    _ => None,
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-    for (x, y) in x_pos {
-        for [dx, dy] in [[1i32, 0i32], [0, -1], [-1, 0], [0, 1]] {
-            if x as i32 + 3 * dx > 0
-                && y as i32 + 3 * dy > 0
-                && ['M', 'A', 'S'].iter().enumerate().all(|(index, letter)| {
-                    input
-                        .get((y as i32 + dy) as usize * index)
-                        .map(|row| {
-                            row.get((x as i32 + dx) as usize * index)
-                                .map(|char| char == letter)
-                        })
-                        .is_some()
-                })
-            {
-                dbg!((x, y, dx, dy));
-                total += 1;
+    for (y, row) in input.iter().enumerate() {
+        for (x, _) in row.iter().enumerate() {
+            for [dx, dy] in [
+                [1, 0],
+                [1, -1],
+                [0, -1],
+                [-1, -1],
+                [-1, 0],
+                [-1, 1],
+                [0, 1],
+                [1, 1],
+            ] {
+                if ['X', 'M', 'A', 'S']
+                    .iter()
+                    .enumerate()
+                    .all(|(position, expected_char)| {
+                        input
+                            .get((y as i32 + (dy * position as i32)) as usize)
+                            .and_then(|row| {
+                                row.get((x as i32 + (dx * position as i32)) as usize)
+                                    .filter(|&char| char == expected_char)
+                            })
+                            .is_some()
+                    })
+                {
+                    total += 1;
+                }
             }
         }
     }
