cd15e781c975888216beaa8e2aaeb110f96aaae4
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-05 06:52:57 +0100

    refactor: day05 cleaned up

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -3,58 +3,42 @@ use std::cmp::Ordering;
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-type Input = [Vec<Vec<u32>>; 2];
+type Input = Vec<Vec<Vec<u32>>>;
 
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    println!("Part 2   test          {} ", part_2(&test_input));
-    println!("         validation    {} ", part_2(&input));
+    let (test_p1, test_p2) = part_1_2(&test_input);
+    let (p1, p2) = part_1_2(&input);
+    println!("Part 1   test          {} ", test_p1);
+    println!("         validation    {} ", p1);
+    println!("Part 2   test          {} ", test_p2);
+    println!("         validation    {} ", p2);
 }
 
-fn part_1(input: &Input) -> u32 {
-    input[1]
-        .iter()
-        .filter_map(|update| {
-            if update.windows(2).all(|p1p2| {
-                input[0]
-                    .iter()
-                    .any(|rule| rule[0] == p1p2[0] && rule[1] == p1p2[1])
-            }) {
-                Some(update[update.len() / 2])
-            } else {
-                None
-            }
-        })
-        .sum()
-}
-
-fn part_2(input: &Input) -> u32 {
-    input[1]
-        .iter()
-        .filter_map(|update| {
-            if update.windows(2).any(|p1p2| {
-                !input[0]
-                    .iter()
-                    .any(|rule| rule[0] == p1p2[0] && rule[1] == p1p2[1])
-            }) {
-                let mut reordered = update.clone();
-                reordered.sort_by(|a, b| {
-                    if input[0].iter().any(|rule| &rule[0] == a && &rule[1] == b) {
-                        Ordering::Greater
-                    } else {
-                        Ordering::Less
-                    }
-                });
-
-                Some(reordered[reordered.len() / 2])
-            } else {
-                None
-            }
-        })
-        .sum()
+fn part_1_2(input: &Input) -> (u32, u32) {
+    let mut p1 = 0;
+    let mut p2 = 0;
+    input[1].iter().for_each(|update| {
+        if update.windows(2).all(|p1p2| {
+            input[0]
+                .iter()
+                .any(|rule| rule[0] == p1p2[0] && rule[1] == p1p2[1])
+        }) {
+            p1 += update[update.len() / 2];
+        } else {
+            let mut reordered = update.clone();
+            reordered.sort_by(|a, b| {
+                if input[0].iter().any(|rule| &rule[0] == a && &rule[1] == b) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            });
+            p2 += reordered[reordered.len() / 2]
+        }
+    });
+    (p1, p2)
 }
 
 fn parse(input: &str) -> Input {
@@ -72,6 +56,4 @@ fn parse(input: &str) -> Input {
                 .collect()
         })
         .collect::<Vec<_>>()
-        .try_into()
-        .unwrap()
 }
