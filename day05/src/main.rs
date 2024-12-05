41ec3a90c60c7233bfe4d43b5849d42eb80185e5
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-05 06:36:54 +0100

    feat: day05 working

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
@@ -9,7 +11,7 @@ fn main() {
     println!("Part 1   test          {} ", part_1(&test_input));
     println!("         validation    {} ", part_1(&input));
     println!("Part 2   test          {} ", part_2(&test_input));
-    // println!("         validation    {} ", part_2(&input));
+    println!("         validation    {} ", part_2(&input));
 }
 
 fn part_1(input: &Input) -> u32 {
@@ -30,7 +32,29 @@ fn part_1(input: &Input) -> u32 {
 }
 
 fn part_2(input: &Input) -> u32 {
-    42
+    input[1]
+        .iter()
+        .filter_map(|update| {
+            if update.windows(2).any(|p1p2| {
+                !input[0]
+                    .iter()
+                    .any(|rule| rule[0] == p1p2[0] && rule[1] == p1p2[1])
+            }) {
+                let mut reordered = update.clone();
+                reordered.sort_by(|a, b| {
+                    if input[0].iter().any(|rule| &rule[0] == a && &rule[1] == b) {
+                        Ordering::Greater
+                    } else {
+                        Ordering::Less
+                    }
+                });
+
+                Some(reordered[reordered.len() / 2])
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn parse(input: &str) -> Input {
