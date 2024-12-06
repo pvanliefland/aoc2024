a060317398022c703a790e07fd8d7f4148a4256e
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-06 06:32:43 +0100

    feat: day06 part 1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,20 +1,63 @@
-const INPUT_TEST: &str = include_str!("../input_test.txt");
-// const INPUT: &str = include_str!("../input.txt");
+use core::panic;
+use std::{
+    collections::{HashMap, HashSet},
+    hash::RandomState,
+};
 
-type Input = u32;
+const INPUT_TEST: &str = include_str!("../input_test.txt");
+const INPUT: &str = include_str!("../input.txt");
+
+type Input = HashMap<(isize, isize), char, RandomState>;
 
 fn main() {
-    let test_input = parse(INPUT_TEST);
-    // let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    // println!("         validation    {} ", part_1(&input));
+    let mut test_input = parse(INPUT_TEST);
+    let mut input = parse(INPUT);
+    println!("Part 1   test          {} ", part_1(&mut test_input));
+    println!("         validation    {} ", part_1(&mut input));
     // println!("Part 2   test          {} ", part_2(&test_input));
     // println!("         validation    {} ", part_2(&input));
 }
 
-fn part_1(input: &Input) -> u32 {
-    dbg!(input);
-    42
+fn part_1(input: &mut Input) -> usize {
+    let mut current = input
+        .iter()
+        .find(|(_, c)| *c == &'^')
+        .map(|(p, c)| (*p, *c))
+        .unwrap();
+    let mut visited = HashSet::new();
+    loop {
+        let (dx, dy) = match current.1 {
+            '^' => (0, -1),
+            '>' => (1, 0),
+            'v' => (0, 1),
+            '<' => (-1, 0),
+            _ => panic!("Oops"),
+        };
+        let next = (current.0 .0 + dx, current.0 .1 + dy);
+        match input.get(&next) {
+            Some('#') => {
+                let next_dir = match current.1 {
+                    '^' => '>',
+                    '>' => 'v',
+                    'v' => '<',
+                    '<' => '^',
+                    _ => panic!("Oops"),
+                };
+                current = (current.0, next_dir);
+            }
+            Some('.') => {
+                visited.insert(current.0);
+                *input.get_mut(&current.0).unwrap() = '.';
+                current = (next, current.1);
+            }
+            Some(_) => panic!("Oops"),
+            None => {
+                visited.insert(current.0);
+                break;
+            }
+        }
+    }
+    visited.len()
 }
 
 // fn part_2(input: &Input) -> u32 {
@@ -22,5 +65,15 @@ fn part_1(input: &Input) -> u32 {
 // }
 
 fn parse(input: &str) -> Input {
-    input.trim().parse::<u32>().unwrap()
+    input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(x, c)| ((x as isize, y as isize), c))
+                .collect::<Vec<_>>()
+        })
+        .collect()
 }
