f3779806ed952b6dad98c99ca48b3e200634c2b4
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-08 00:09:37 +0100

    refactor: totally not worth it

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,133 +1,119 @@
 use core::panic;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-type Position = (isize, isize);
-type Input = (HashMap<Position, char>, (Position, char));
+type Input = (((usize, usize), char), usize, Vec<Vec<char>>);
 
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
     let test_visited = part_1(&test_input);
     let visited = part_1(&input);
-    println!("Part 1   test     {}", test_visited.len());
-    println!("         actual   {}", visited.len());
-    println!("Part 2   test     {} ", part_2(&test_input, test_visited));
-    println!("         actual   {} ", part_2(&input, visited));
+    println!("Part 1   test          {} ", test_visited.len());
+    println!("         validation    {} ", visited.len());
+    println!(
+        "Part 2   test          {} ",
+        part_2(&test_input, test_visited)
+    );
+    println!("         validation    {} ", part_2(&input, visited));
 }
 
-fn part_1(input: &Input) -> HashSet<Position> {
-    let (map, (mut pos, mut dir)) = input;
-    let mut visited = HashSet::new();
+fn part_1(input: &Input) -> HashSet<(usize, usize)> {
+    let ((mut pos, mut dir), max_xy, map) = input;
+    let mut visited = HashSet::with_capacity(max_xy.pow(2));
     loop {
-        match step(map, pos, dir, None) {
-            Outcome::Move(next_pos) => {
-                visited.insert(pos);
-                pos = next_pos;
-            }
-            Outcome::Turn(next_dir) => {
-                dir = next_dir;
-            }
-            Outcome::GetOut => {
-                visited.insert(pos);
-                break;
-            }
+        visited.insert(pos);
+        if let Some((next_pos, next_dir)) = step(pos, dir, *max_xy, map, None) {
+            pos = next_pos;
+            dir = next_dir;
+        } else {
+            break;
         }
     }
     visited
 }
 
-fn part_2(input: &Input, part_1: HashSet<(isize, isize)>) -> usize {
-    let (map, (start_pos, start_dir)) = input;
+fn part_2(input: &Input, obstacles: HashSet<(usize, usize)>) -> usize {
+    let ((start_pos, start_dir), max_xy, map) = input;
     let mut loops = 0;
-    for candidate in part_1 {
+    for obstacle in obstacles {
+        let (mut pos, mut dir) = (*start_pos, *start_dir);
         let mut visited = HashSet::new();
-        let (mut pos, mut dir) = (*start_pos, *start_dir).to_owned();
         loop {
-            match step(map, pos, dir, Some(candidate)) {
-                Outcome::Move(next_pos) => {
-                    if visited.contains(&(pos, dir)) {
-                        loops += 1;
-                        break;
-                    }
-                    visited.insert((pos, dir));
-                    pos = next_pos;
-                }
-                Outcome::Turn(next_dir) => {
-                    dir = next_dir;
-                }
-                Outcome::GetOut => {
-                    visited.insert((pos, dir));
-                    break;
-                }
+            if visited.contains(&(pos, dir)) {
+                loops += 1;
+                break;
+            }
+            visited.insert((pos, dir));
+            if let Some((next_pos, next_dir)) = step(pos, dir, *max_xy, map, Some(obstacle)) {
+                pos = next_pos;
+                dir = next_dir;
+            } else {
+                break;
             }
         }
     }
+
     loops
 }
 
 fn step(
-    map: &HashMap<Position, char>,
-    pos: Position,
+    pos: (usize, usize),
     dir: char,
-    obstacle: Option<Position>,
-) -> Outcome {
-    let (dx, dy) = match dir {
-        '^' => (0, -1),
-        '>' => (1, 0),
-        'v' => (0, 1),
-        '<' => (-1, 0),
-        _ => panic!("Oops"),
+    max_xy: usize,
+    map: &Vec<Vec<char>>,
+    obstacle: Option<(usize, usize)>,
+) -> Option<((usize, usize), char)> {
+    let next_pos = match (pos, dir) {
+        ((_, 0), '^') | ((0, _), '<') => None,
+        ((x, y), '^') => Some((x, y - 1)),
+        ((x, _), '>') if x == max_xy => None,
+        ((x, y), '>') => Some((x + 1, y)),
+        ((_, y), 'v') if y == max_xy => None,
+        ((x, y), 'v') => Some((x, y + 1)),
+        ((x, y), '<') => Some((x - 1, y)),
+        _ => panic!("oops"),
     };
-    let next_pos = (pos.0 + dx, pos.1 + dy);
-    let at_pos = if Some(next_pos) == obstacle {
-        Some(&'#')
-    } else {
-        map.get(&next_pos)
-    };
-    match at_pos {
-        Some('#') => Outcome::Turn(match dir {
-            '^' => '>',
-            '>' => 'v',
-            'v' => '<',
-            '<' => '^',
+    if let Some(next_pos) = next_pos {
+        let at_pos = if Some(next_pos) == obstacle {
+            '#'
+        } else {
+            *map[next_pos.1].get(next_pos.0).unwrap()
+        };
+        match at_pos {
+            '#' => step(
+                pos,
+                match dir {
+                    '^' => '>',
+                    '>' => 'v',
+                    'v' => '<',
+                    '<' => '^',
+                    _ => panic!("Oops"),
+                },
+                max_xy,
+                map,
+                obstacle,
+            ),
+            '.' | '^' | '>' | 'v' | '<' => Some((next_pos, dir)),
             _ => panic!("Oops"),
-        }),
-        Some('.' | '^' | '>' | 'v' | '<') => Outcome::Move(next_pos),
-        Some(_) => panic!("Oops"),
-        None => Outcome::GetOut,
+        }
+    } else {
+        None
     }
 }
 
-enum Outcome {
-    Turn(char),
-    Move(Position),
-    GetOut,
-}
-
 fn parse(input: &str) -> Input {
-    let mut start = ((0, 0), '!');
-    (
-        input
-            .trim()
-            .lines()
-            .enumerate()
-            .flat_map(|(y, row)| {
-                row.chars()
-                    .enumerate()
-                    .map(|(x, c)| {
-                        let x = isize::try_from(x).unwrap();
-                        let y = isize::try_from(y).unwrap();
-                        if c == '^' {
-                            start = ((x, y), '^');
-                        }
-                        ((x, y), c)
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .collect(),
-        start,
-    )
+    let map: Vec<_> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+    let start = map
+        .iter()
+        .enumerate()
+        .flat_map(move |(y, row)| row.iter().enumerate().map(move |(x, c)| ((x, y), *c)))
+        .find(|(_, c)| c == &'^')
+        .unwrap();
+    (start, map.len() - 1, map)
 }
