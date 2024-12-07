c2ec4404be47183001c6dd1a067b5e5806853673
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-08 00:28:11 +0100

    refactor: day07

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -6,43 +6,49 @@ type Input = Vec<(usize, Vec<usize>)>;
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input, false));
-    println!("         validation    {} ", part_1(&input, false));
-    println!("Part 2   test          {} ", part_1(&test_input, true));
-    println!("         validation    {} ", part_1(&input, true));
+    let (test_part_1, test_part_2) = part_1_2(&test_input);
+    let (part_1, part_2) = part_1_2(&input);
+    println!("Part 1   test          {test_part_1}");
+    println!("         validation    {part_1}");
+    println!("Part 2   test          {test_part_2}");
+    println!("         validation    {part_2}");
 }
 
-fn part_1(input: &Input, concat: bool) -> usize {
-    input
-        .iter()
-        .filter_map(|(left, right)| {
-            let combinations = combinations(right.len() - 1, concat);
-            if combinations.iter().any(|combo| {
-                let mut result = right[0];
-                for (index, op) in combo.iter().enumerate() {
-                    match op {
-                        '+' => {
-                            result += right[index + 1];
-                        }
-                        '*' => {
-                            result *= right[index + 1];
-                        }
-                        '|' => {
-                            result = (result.to_string() + &right[index + 1].to_string())
-                                .parse()
-                                .unwrap();
-                        }
-                        _ => panic!("oops"),
-                    }
+fn part_1_2(input: &Input) -> (usize, usize) {
+    let mut without_concat = 0;
+    let mut with_concat = 0;
+    for (left, right) in input {
+        if validate(*left, right, false) {
+            without_concat += left;
+        } else if validate(*left, right, true) {
+            with_concat += left;
+        }
+    }
+    (without_concat, with_concat + without_concat)
+}
+
+fn validate(left: usize, right: &[usize], concat: bool) -> bool {
+    let combinations = combinations(right.len() - 1, concat);
+    combinations.iter().any(|combo| {
+        let mut result = right[0];
+        for (index, op) in combo.iter().enumerate() {
+            match op {
+                '+' => {
+                    result += right[index + 1];
                 }
-                left == &result
-            }) {
-                Some(left)
-            } else {
-                None
+                '*' => {
+                    result *= right[index + 1];
+                }
+                '|' => {
+                    result = (result.to_string() + &right[index + 1].to_string())
+                        .parse()
+                        .unwrap();
+                }
+                _ => panic!("oops"),
             }
-        })
-        .sum()
+        }
+        left == result
+    })
 }
 
 fn combinations(size: usize, concat: bool) -> Vec<Vec<char>> {
@@ -63,10 +69,6 @@ fn combinations(size: usize, concat: bool) -> Vec<Vec<char>> {
     }
     result
 }
-
-// fn part_2(input: &Input) -> u32 {
-//     input.trim().parse::<u32>().unwrap()
-// }
 
 fn parse(input: &str) -> Input {
     input
