871f6722c6de955fe29bfde1df1033b51144f581
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-08 09:58:13 +0100

    refactor: day08, nicer

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -16,64 +16,39 @@ fn main() {
 
 fn part_1(input: &Input) -> usize {
     let (grid, size) = input;
-    let mut antinodes = HashSet::new();
-    for (freq, positions) in grid {
-        if freq == &'.' {
-            continue;
-        }
-        let combinations = combinations(positions, 2);
-        for combination in combinations {
-            let (p1, p2) = (combination[0], combination[1]);
-            let (dx, dy) = ((p2.0 - p1.0), (p2.1 - p1.1));
-            let antinode_1 = (p1.0 + 2 * dx, p1.1 + 2 * dy);
-            let antinode_2 = (p2.0 - 2 * dx, p2.1 - 2 * dy);
-
-            if antinode_1.0 >= 0
-                && antinode_1.0 < *size
-                && antinode_1.1 >= 0
-                && antinode_1.1 < *size
-            {
-                antinodes.insert(antinode_1);
-            }
-            if antinode_2.0 >= 0
-                && antinode_2.0 < *size
-                && antinode_2.1 >= 0
-                && antinode_2.1 < *size
-            {
-                antinodes.insert(antinode_2);
-            }
-        }
-    }
-
-    antinodes.len()
+    count_antinodes(grid, *size, Some(2))
 }
 
 fn part_2(input: &Input) -> usize {
     let (grid, size) = input;
+    count_antinodes(grid, *size, None)
+}
+
+fn count_antinodes(
+    grid: &HashMap<char, Vec<(isize, isize)>>,
+    size: isize,
+    distance: Option<isize>,
+) -> usize {
     let mut antinodes = HashSet::new();
-    for (freq, positions) in grid {
-        if freq == &'.' {
-            continue;
-        }
+    for positions in grid.values() {
         let combinations = combinations(positions, 2);
         for combination in combinations {
             let (p1, p2) = (combination[0], combination[1]);
             let (dx, dy) = ((p2.0 - p1.0), (p2.1 - p1.1));
 
-            #[allow(clippy::maybe_infinite_iter)]
-            (0..)
-                .map(|step| ((p1.0 + step * dx), (p1.1 + step * dy)))
-                .take_while(|&(x, y)| x >= 0 && x < *size && y >= 0 && y < *size)
-                .for_each(|antinode| {
-                    antinodes.insert(antinode);
-                });
-            #[allow(clippy::maybe_infinite_iter)]
-            (0..)
-                .map(|step| ((p1.0 - step * dx), (p1.1 - step * dy)))
-                .take_while(|&(x, y)| x >= 0 && x < *size && y >= 0 && y < *size)
-                .for_each(|antinode| {
-                    antinodes.insert(antinode);
-                });
+            for (pos, dir) in [(p1, 1), (p2, -1)] {
+                let range = if let Some(distance) = distance {
+                    distance..=distance
+                } else {
+                    0..=isize::MAX
+                };
+                range
+                    .map(|step| ((pos.0 + step * dx * dir), (pos.1 + step * dy * dir)))
+                    .take_while(|&(x, y)| x >= 0 && x < size && y >= 0 && y < size)
+                    .for_each(|antinode| {
+                        antinodes.insert(antinode);
+                    });
+            }
         }
     }
 
@@ -107,13 +82,16 @@ fn combinations<T: Copy + Clone + Eq>(items: &[T], size: usize) -> Vec<Vec<T>> {
 fn parse(input: &str) -> Input {
     let mut grid: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
     input.trim().lines().enumerate().for_each(|(y, line)| {
-        line.chars().enumerate().for_each(|(x, c)| {
-            grid.entry(c)
-                .and_modify(|positions| {
-                    positions.push((x as isize, y as isize));
-                })
-                .or_insert(vec![(x as isize, y as isize)]);
-        });
+        line.chars()
+            .enumerate()
+            .filter(|(_, c)| c != &'.')
+            .for_each(|(x, c)| {
+                grid.entry(c)
+                    .and_modify(|positions| {
+                        positions.push((x as isize, y as isize));
+                    })
+                    .or_insert(vec![(x as isize, y as isize)]);
+            });
     });
     (grid, input.trim().lines().count() as isize)
 }
