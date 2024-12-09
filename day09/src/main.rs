0137b1f3ee031259cd1fc99e74f6a501478a1551
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-09 20:23:43 +0100

    refactor: day09, cleaner

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,67 +1,74 @@
+use std::fmt::DebugSet;
+
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-type Input = Vec<Option<usize>>;
-type Input2 = Vec<Chunk>;
+type DiskLayout = Vec<Block>;
 
 #[derive(Copy, Clone, Debug)]
-enum Chunk {
+enum Block {
     File(usize, usize),
     Free(usize),
 }
 
 fn main() {
-    let test_input = parse(INPUT_TEST);
-    let input = parse(INPUT);
-    let test_input_2 = parse_2(INPUT_TEST);
-    let input_2 = parse_2(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    println!("Part 2   test          {} ", part_2(&test_input_2));
-    println!("         validation    {} ", part_2(&input_2));
+    let (small_test_layout, big_test_layout) = parse(INPUT_TEST);
+    let (small_layout, big_layout) = parse(INPUT);
+    println!("Part 1   test          {} ", part_1(&small_test_layout));
+    println!("         validation    {} ", part_1(&small_layout));
+    println!("Part 2   test          {} ", part_2(&big_test_layout));
+    println!("         validation    {} ", part_2(&big_layout));
 }
 
-fn part_1(input: &Input) -> usize {
+fn part_1(input: &DiskLayout) -> usize {
     let mut blocks = input.clone();
     let mut head = 0;
     let mut tail = input.len() - 1;
     loop {
         if head >= tail {
             break;
-        } else if blocks[head].is_none() && blocks[tail].is_some() {
-            blocks.swap(head, tail);
-            head += 1;
-            tail -= 1;
-        } else if blocks[head].is_none() {
-            tail -= 1;
-        } else if blocks[head].is_some() {
-            head += 1;
+        }
+        match (blocks[head], blocks[tail]) {
+            (Block::Free(_), Block::File(_, _)) => {
+                blocks.swap(head, tail);
+                head += 1;
+                tail -= 1;
+            }
+            (Block::Free(_), Block::Free(_)) => {
+                tail -= 1;
+            }
+            (Block::File(_, _), _) => {
+                head += 1;
+            }
         }
     }
     blocks
         .iter()
         .enumerate()
-        .filter_map(|(i, b)| b.map(|b| b * i))
+        .filter_map(|(i, b)| match b {
+            Block::File(id, _) => Some(i * id),
+            _ => None,
+        })
         .sum()
 }
 
-fn part_2(input: &Input2) -> usize {
+fn part_2(input: &DiskLayout) -> usize {
     let mut blocks = input.clone();
     let mut tail = input.len() - 1;
     while tail > 1 {
         match blocks[tail] {
-            Chunk::File(_, file_size) => {
+            Block::File(_, file_size) => {
                 if let Some((head, free_size)) = blocks
                     .iter()
                     .enumerate()
                     .filter_map(|(i, b)| match b {
-                        Chunk::Free(size) => Some((i, *size)),
-                        Chunk::File(_, _) => None,
+                        Block::Free(size) => Some((i, *size)),
+                        Block::File(_, _) => None,
                     })
                     .find(|(i, free_size)| i < &tail && free_size >= &file_size)
                 {
                     let (free1, free2) =
-                        (Chunk::Free(free_size - file_size), Chunk::Free(file_size));
+                        (Block::Free(free_size - file_size), Block::Free(file_size));
                     blocks.remove(head);
                     blocks.insert(head, blocks[tail - 1]);
                     blocks.remove(tail);
@@ -70,7 +77,7 @@ fn part_2(input: &Input2) -> usize {
                 }
                 tail -= 1;
             }
-            Chunk::Free(_) => {
+            Block::Free(_) => {
                 tail -= 1;
             }
         }
@@ -78,8 +85,8 @@ fn part_2(input: &Input2) -> usize {
     blocks
         .iter()
         .flat_map(|b| match b {
-            Chunk::File(id, size) => vec![Some(id); *size],
-            Chunk::Free(size) => vec![None; *size],
+            Block::File(id, size) => vec![Some(id); *size],
+            Block::Free(size) => vec![None; *size],
         })
         .enumerate()
         .filter_map(|(i, b)| b.map(|b| b * i))
@@ -87,45 +94,33 @@ fn part_2(input: &Input2) -> usize {
 }
 
 #[allow(unused)]
-fn debug(blocks: &[Chunk]) {
+fn debug(blocks: &[Block]) {
     println!(
         "{}",
         blocks
             .iter()
             .map(|b| match b {
-                Chunk::File(id, size) =>
+                Block::File(id, size) =>
                     vec![id.to_string(); *size].into_iter().collect::<String>(),
-                Chunk::Free(size) => vec![".".to_string(); *size].into_iter().collect::<String>(),
+                Block::Free(size) => vec![".".to_string(); *size].into_iter().collect::<String>(),
             })
             .collect::<String>()
     );
 }
 
-fn parse(input: &str) -> Input {
-    input
-        .trim()
-        .chars()
-        .enumerate()
-        .flat_map(|(i, c)| {
-            if i % 2 == 0 {
-                vec![Some(i / 2); c.to_digit(10).unwrap() as usize]
-            } else {
-                vec![None; c.to_digit(10).unwrap() as usize]
-            }
-        })
-        .collect()
-}
-fn parse_2(input: &str) -> Input2 {
-    input
-        .trim()
-        .chars()
-        .enumerate()
-        .map(|(i, c)| {
-            if i % 2 == 0 {
-                Chunk::File(i / 2, c.to_digit(10).unwrap() as usize)
-            } else {
-                Chunk::Free(c.to_digit(10).unwrap() as usize)
-            }
-        })
-        .collect()
+fn parse(input: &str) -> (DiskLayout, DiskLayout) {
+    let (mut small_blocks, mut big_blocks) = (vec![], vec![]);
+    input.trim().chars().enumerate().for_each(|(i, b)| {
+        if i % 2 == 0 {
+            small_blocks.extend(vec![
+                Block::File(i / 2, 1);
+                b.to_digit(10).unwrap() as usize
+            ]);
+            big_blocks.push(Block::File(i / 2, b.to_digit(10).unwrap() as usize));
+        } else {
+            small_blocks.extend(vec![Block::Free(1); b.to_digit(10).unwrap() as usize]);
+            big_blocks.push(Block::Free(b.to_digit(10).unwrap() as usize));
+        }
+    });
+    (small_blocks, big_blocks)
 }
