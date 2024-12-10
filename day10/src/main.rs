c21fa5bed9ce122dcca089731f388866528edb3e
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-10 06:54:15 +0100

    refactor: day10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
@@ -9,30 +9,18 @@ type Map = HashMap<Position, u8>;
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    println!("Part 2   test          {} ", part_2(&test_input));
-    println!("         validation    {} ", part_2(&input));
+    println!("Part 1   test          {} ", ratings(&test_input, false));
+    println!("         validation    {} ", ratings(&input, false));
+    println!("Part 2   test          {} ", ratings(&test_input, true));
+    println!("         validation    {} ", ratings(&input, true));
 }
 
-fn part_1(input: &Map) -> u32 {
+fn ratings(input: &Map, distinct: bool) -> u32 {
     input
         .iter()
         .filter_map(|(pos, height)| {
             if height == &0 {
-                Some(explore(*pos, input))
-            } else {
-                None
-            }
-        })
-        .sum()
-}
-fn part_2(input: &Map) -> u32 {
-    input
-        .iter()
-        .filter_map(|(pos, height)| {
-            if height == &0 {
-                Some(explore_2(*pos, input))
+                Some(explore(*pos, input, distinct))
             } else {
                 None
             }
@@ -40,43 +28,21 @@ fn part_2(input: &Map) -> u32 {
         .sum()
 }
 
-fn explore(root: Position, map: &Map) -> u32 {
+fn explore(root: Position, map: &Map, distinct: bool) -> u32 {
     let mut total = 0;
-    let mut explored = vec![root];
-    let mut queue = vec![(root, 0)];
+    let mut explored: HashSet<Position> = HashSet::from_iter(vec![root]);
+    let mut queue = VecDeque::from(vec![(root, 0)]);
     while !queue.is_empty() {
-        let (pos, height) = queue.remove(0);
+        let (pos, height) = queue.pop_front().unwrap();
         if height == 9 {
             total += 1;
         }
         for step in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
             let edge_pos = (pos.0 + step.0, pos.1 + step.1);
             if let Some(edge_height) = map.get(&edge_pos) {
-                if *edge_height == height + 1 && !explored.contains(&edge_pos) {
-                    explored.push(edge_pos);
-                    queue.push((edge_pos, *edge_height));
-                }
-            }
-        }
-    }
-    total
-}
-
-fn explore_2(root: Position, map: &Map) -> u32 {
-    let mut total = 0;
-    let mut explored = vec![root];
-    let mut queue = vec![(root, 0)];
-    while !queue.is_empty() {
-        let (pos, height) = queue.remove(0);
-        if height == 9 {
-            total += 1;
-        }
-        for step in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
-            let edge_pos = (pos.0 + step.0, pos.1 + step.1);
-            if let Some(edge_height) = map.get(&edge_pos) {
-                if *edge_height == height + 1 {
-                    explored.push(edge_pos);
-                    queue.push((edge_pos, *edge_height));
+                if *edge_height == height + 1 && (distinct || !explored.contains(&edge_pos)) {
+                    explored.insert(edge_pos);
+                    queue.push_back((edge_pos, *edge_height));
                 }
             }
         }
