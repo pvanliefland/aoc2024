25517ec2f0f4d449dec52c04b8e061824b1b2383
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-12 23:17:53 +0100

    feat: wtf it works

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     hash::RandomState,
+    ops::ControlFlow,
 };
 
 const INPUT_TEST: &str = include_str!("../input_test.txt");
@@ -13,23 +14,26 @@ type Graph = HashMap<Position, Vec<Position>>;
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    // println!("Part 2   test          {} ", part_2(&test_input));
-    // println!("         validation    {} ", part_2(&input));
+    // println!("Part 1   test          {} ", part_1(&test_input).0);
+    // println!("         validation    {} ", part_1(&input));
+    println!("Part 2   test          {} ", part_1(&test_input).1);
+    println!("         validation    {} ", part_1(&input).1);
 }
 
-fn part_1(input: &(Map, Graph)) -> usize {
-    let mut price = 0;
+fn part_1(input: &(Map, Graph)) -> (usize, usize) {
+    let (mut price, mut discount_price) = (0, 0);
     let (map, graph) = input;
     let mut all_explored: HashSet<Position> = HashSet::new();
-    for pos in map.keys() {
+    for (pos, c) in map {
         if !all_explored.contains(pos) {
-            let (area, perimeter) = explore(map, graph, *pos, &mut all_explored);
+            // pos is a root
+            let (area, perimeter, sides) = explore(map, graph, (*pos, *c), &mut all_explored);
+            println!("{}: {}", c, sides);
             price += area * perimeter;
+            discount_price += area * sides;
         }
     }
-    price
+    (price, discount_price)
 }
 
 // fn part_2(input: &Input) -> u32 {
@@ -39,24 +43,64 @@ fn part_1(input: &(Map, Graph)) -> usize {
 fn explore(
     map: &Map,
     graph: &Graph,
-    root: Position,
+    root: (Position, char),
     all_explored: &mut HashSet<Position>,
-) -> (usize, usize) {
+) -> (usize, usize, usize) {
+    let (root_pos, root_c) = root;
+    let (mut min_x, mut max_x, mut min_y, mut max_y) =
+        (root_pos.0, root_pos.0, root_pos.1, root_pos.1);
     let mut perimeter = 0;
-    let mut queue = VecDeque::from_iter(vec![root]);
-    let mut explored: HashSet<(isize, isize), RandomState> = HashSet::from_iter(vec![root]);
+    let mut queue = VecDeque::from_iter(vec![root_pos]);
+    let mut explored: HashSet<(isize, isize), RandomState> = HashSet::from_iter(vec![root_pos]);
     while let Some(pos) = queue.pop_front() {
         let neighbors = graph.get(&pos).unwrap();
         perimeter += 4 - neighbors.len();
         for adj in graph.get(&pos).unwrap() {
             if !explored.contains(adj) {
+                min_x = min_x.min(adj.0);
+                max_x = max_x.max(adj.0);
+                min_y = min_y.min(adj.1);
+                max_y = max_y.max(adj.1);
                 explored.insert(*adj);
                 all_explored.insert(*adj);
                 queue.push_back(*adj);
             }
         }
     }
-    (explored.len(), perimeter)
+
+    // let's cast some rays now lol
+    let mut sides = 0;
+    let mut x_collisions = vec![];
+    for y in min_y..=max_y {
+        let mut inside = false;
+        for x in min_x..=max_x + 1 {
+            let next_inside = explored.contains(&(x, y)) && map.get(&(x, y)) == Some(&root_c);
+            if next_inside != inside {
+                inside = next_inside;
+                x_collisions.push((x, y, inside));
+                if !x_collisions.contains(&(x, y - 1, inside)) {
+                    println!("x coll at {},{}", x, y);
+                    sides += 1;
+                }
+            }
+        }
+    }
+    let mut y_collisions = vec![];
+    for x in min_x..=max_x {
+        let mut inside = false;
+        for y in min_y..=max_y + 1 {
+            let next_inside = explored.contains(&(x, y)) && map.get(&(x, y)) == Some(&root_c);
+            if next_inside != inside {
+                inside = next_inside;
+                y_collisions.push((x, y, inside));
+                if !y_collisions.contains(&(x - 1, y, inside)) {
+                    println!("y coll at {},{}", x, y);
+                    sides += 1;
+                }
+            }
+        }
+    }
+    (explored.len(), perimeter, sides)
 }
 
 fn parse(input: &str) -> (Map, Graph) {
