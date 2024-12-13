dbe43100aa942d7417afecffbcd67f2797c54424
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-13 22:25:39 +0100

    refactor: day13 but it works

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
@@ -9,53 +7,44 @@ type Input = Vec<Machine>;
 fn main() {
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    // println!("Part 2   test          {} ", part_2(&test_input));
-    // println!("         validation    {} ", part_2(&input));
+    println!("Part 1   test          {} ", part_1(&test_input, 0));
+    println!("         validation    {} ", part_1(&input, 0));
+    println!(
+        "Part 2   test          {} ",
+        part_1(&test_input, 10000000000000)
+    );
+    println!("         validation    {} ", part_1(&input, 10000000000000));
 }
 
-fn part_1(input: &Input) -> isize {
+fn part_1(input: &Input, dp: isize) -> isize {
     input
         .iter()
         .map(|machine| {
-            let x_values = (0..100)
-                .filter_map(|x1| {
-                    let dividend = -machine[0][0] * x1 + machine[2][0];
-                    let divisor = machine[1][0];
-                    if dividend % divisor == 0 {
-                        let x2 = dividend / divisor;
-                        if (0..=100).contains(&x2) {
-                            Some((x1, x2))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .collect::<HashSet<(isize, isize)>>();
-            let y_values = (0..100)
-                .filter_map(|y1| {
-                    let dividend = -machine[0][1] * y1 + machine[2][1];
-                    let divisor = machine[1][1];
-                    if dividend % divisor == 0 {
-                        let y2 = dividend / divisor;
-                        if (0..=100).contains(&y2) {
-                            Some((y1, y2))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .collect::<HashSet<(isize, isize)>>();
-            let cheapest = x_values
-                .intersection(&y_values)
-                .map(|(a, b)| ((a, b), 3 * a + b))
-                .min_by_key(|(_, c)| *c);
-            cheapest.map(|(_, c)| c).unwrap_or(0)
+            let (x1, x2) = (machine[0][0] as f64, machine[1][0] as f64);
+            let (y1, y2) = (machine[0][1] as f64, machine[1][1] as f64);
+            let (px, py) = ((machine[2][0] + dp) as f64, (machine[2][1] + dp) as f64);
+            // we now that a * x1 + b * x2 = px
+            //    and that a * y1 + b * y2 = py
+            // so b = (px - a * x1) / x2
+            // so a * y1 + ((px - a * x1) / x2) * y2 = py
+            // so a * y1 / y2 + ((px - a * x1) / x2) = py / y2
+            // so a * y1 / y2 + px / x2 - a * x1 / x2 = py / y2
+            // so a * y1 / y2 - a * x1 / x2 = py / y2 - px / x2
+            // so a * (y1 / y2 - x1 / x2) = py / y2 - px / x2
+            // so a = (py / y2 - px / x2) / (y1 / y2 - x1 / x2)
+            let a = (py / y2 - px / x2) / (y1 / y2 - x1 / x2);
+            let b = (px - a * x1) / x2;
+            if (a.fract() < 0.01 || a.fract() > 0.99) && (b.fract() < 0.01 || b.fract() > 0.99) {
+                let really_ok = (a.round() * x1 + b.round() * x2 == px)
+                    && (a.round() * y1 + b.round() * y2 == py);
+                if really_ok {
+                    return 3 * a.round() as isize + b.round() as isize;
+                } else {
+                    dbg!("here");
+                    return 0;
+                }
+            }
+            0
         })
         .sum()
 }
