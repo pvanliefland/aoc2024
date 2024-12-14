d8fa0114c38d4c859a97cd81e8ba421d9d01fa5a
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-14 15:46:47 +0100

    feat: day11 part 2, at last!

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,53 +1,59 @@
+use std::{collections::HashMap, time::Instant};
+
 const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-type Input = Vec<String>;
+type Stone = (usize, usize);
+type Input = Vec<Stone>;
 
 fn main() {
+    let start = Instant::now();
     let test_input = parse(INPUT_TEST);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
-    println!("         validation    {} ", part_1(&input));
-    // println!("Part 2   test          {} ", part_2(&test_input));
-    // println!("         validation    {} ", part_2(&input));
+    println!("Part 1   test          {} ", count_stones(&test_input, 25));
+    println!("         validation    {} ", count_stones(&input, 25));
+    println!("Part 2   test          {} ", count_stones(&test_input, 75));
+    println!("         validation    {} ", count_stones(&input, 75));
+    println!("Duration: {:?}", start.elapsed());
 }
 
-fn part_1(input: &Input) -> usize {
+fn count_stones(input: &Input, blinks: usize) -> usize {
     let mut transformed = input.clone();
-    for _ in 0..25 {
+    for _ in 0..blinks {
         transformed = step(&transformed);
-        // println!("{}", transformed.join(" "));
+        let mut map = HashMap::new();
+        for stone in &transformed {
+            map.entry(stone.0)
+                .and_modify(|count| *count += stone.1)
+                .or_insert(stone.1);
+        }
+        transformed = map.into_iter().collect();
     }
-    transformed.len()
+    transformed.iter().map(|s| s.1).sum()
 }
-
-// fn part_2(input: &Input) -> u32 {
-//     input.trim().parse::<u32>().unwrap()
-// }
 
 fn step(input: &Input) -> Input {
     input
         .iter()
-        .flat_map(|stone| match stone.as_str() {
-            "0" => vec!["1".to_string()],
-            stone if stone.len() % 2 == 0 => {
-                let (left, right) = stone.split_at(stone.len() / 2);
-                vec![left.to_string(), right.to_string()]
-                    .into_iter()
-                    .map(|s| {
-                        if s.parse::<usize>().unwrap() == 0 {
-                            "0".to_string()
-                        } else {
-                            s.trim_start_matches('0').to_string()
-                        }
-                    })
-                    .collect()
+        .flat_map(|stone| match stone.0 {
+            0 => vec![(1, stone.1)],
+            num if num.ilog10() % 2 == 1 => {
+                let as_string = num.to_string();
+                let (left, right) = as_string.split_at(as_string.len() / 2);
+                vec![
+                    (left.parse().unwrap(), stone.1),
+                    (right.parse().unwrap(), stone.1),
+                ]
             }
-            _ => vec![(stone.parse::<usize>().unwrap() * 2024).to_string()],
+            _ => vec![(stone.0 * 2024, stone.1)],
         })
         .collect()
 }
 
 fn parse(input: &str) -> Input {
-    input.trim().split(" ").map(|s| s.to_string()).collect()
+    input
+        .trim()
+        .split(" ")
+        .map(|s| (s.parse().unwrap(), 1))
+        .collect()
 }
