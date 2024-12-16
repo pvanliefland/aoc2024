9fe6c3fd2db47fd6d9e156d7e80a8fb100b995af
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-16 21:23:25 +0100

    feat: day 16 part 1 working

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -4,7 +4,8 @@ use std::{
     time::Instant,
 };
 
-const INPUT_TEST: &str = include_str!("../input_test.txt");
+const INPUT_TEST_1: &str = include_str!("../input_test_1.txt");
+const INPUT_TEST_2: &str = include_str!("../input_test_2.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 type Position = (isize, isize);
@@ -13,9 +14,11 @@ type Input = (Map, Position);
 
 fn main() {
     let start = Instant::now();
-    let test_input = parse(INPUT_TEST);
+    let test_input_1 = parse(INPUT_TEST_1);
+    let test_input_2 = parse(INPUT_TEST_2);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
+    println!("Part 1   test 1        {} ", part_1(&test_input_1));
+    println!("Part 1   test 2        {} ", part_1(&test_input_2));
     println!("         validation    {} ", part_1(&input));
     // println!("Part 2   test          {} ", part_2(&test_input));
     // println!("         validation    {} ", part_2(&input));
@@ -25,7 +28,7 @@ fn main() {
 fn part_1(input: &Input) -> usize {
     let (map, start) = input;
     let mut queue = VecDeque::from_iter(vec![Action::Move('>', *start, 0)]);
-    let mut explored: HashSet<Position> = HashSet::from_iter(vec![*start]);
+    let mut explored: HashSet<(char, Position)> = HashSet::from_iter(vec![('>', *start)]);
     let mut end_move = None;
     while let Some(action) = queue.pop_front() {
         match action {
@@ -38,19 +41,19 @@ fn part_1(input: &Input) -> usize {
                 for mov in [(1, 0, '>'), (0, 1, 'v'), (-1, 0, '<'), (0, -1, '^')] {
                     let adj_pos = (pos.0 + mov.0, pos.1 + mov.1);
                     let at_adj_pos = map.get(&adj_pos).unwrap();
-                    if at_adj_pos != &'#' && !explored.contains(&adj_pos) {
-                        explored.insert(adj_pos);
+                    if at_adj_pos != &'#' && !explored.contains(&(mov.2, adj_pos)) {
                         if dir == mov.2 {
+                            explored.insert((mov.2, adj_pos));
                             queue.push_back(Action::Move(mov.2, adj_pos, cost + 1));
                         } else {
-                            queue.push_back(Action::WaitAndMove(999, mov.2, adj_pos, cost + 1));
+                            queue.push_back(Action::WaitAndMove(1000, mov.2, adj_pos, cost + 1));
                         }
                     }
                 }
             }
             Action::WaitAndMove(time, dir, pos, cost) => {
                 if time == 0 {
-                    queue.push_back(Action::Move(dir, pos, cost + 1));
+                    queue.push_back(Action::Move(dir, pos, cost));
                 } else {
                     queue.push_back(Action::WaitAndMove(time - 1, dir, pos, cost + 1));
                 }
@@ -58,10 +61,7 @@ fn part_1(input: &Input) -> usize {
         }
     }
     match end_move {
-        Some(Action::Move(dir, pos, cost)) => {
-            dbg!(dir, pos);
-            cost
-        }
+        Some(Action::Move(_dir, _pos, cost)) => cost,
         _ => panic!("Oops"),
     }
 }
@@ -83,7 +83,8 @@ fn parse(input: &str) -> Input {
         .lines()
         .enumerate()
         .flat_map(|(y, row)| {
-            row.chars()
+            row.trim()
+                .chars()
                 .enumerate()
                 .map(|(x, c)| {
                     if c == 'S' {
