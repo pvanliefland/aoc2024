b24d2e33e7ee85dfb619573b18ed9a74dde5997f
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-18 18:16:41 +0100

    refactor: day15 ready for part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,8 +1,10 @@
 use core::panic;
 use std::{collections::HashMap, time::Instant};
 
-const INPUT_TEST: &str = include_str!("../input_test.txt");
-const INPUT_TEST_PART_2_SIMPLE: &str = include_str!("../input_test_part2_simple.txt");
+const INPUT_TEST_1: &str = include_str!("../input_test_1.txt");
+const INPUT_TEST_2: &str = include_str!("../input_test_2.txt");
+// const INPUT_TEST_3: &str = include_str!("../input_test_3.txt");
+// const INPUT_TEST_PART_2_SIMPLE: &str = include_str!("../input_test_part2_simple.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 type Position = (isize, isize);
@@ -12,23 +14,27 @@ type Input = (Map, Position, (isize, isize), Vec<Move>);
 
 fn main() {
     let start = Instant::now();
-    let test_input = parse(INPUT_TEST, false);
-    let test_input_part2_simple = parse(INPUT_TEST_PART_2_SIMPLE, true);
+    let test_input_1 = parse(INPUT_TEST_1, false);
+    let test_input_2 = parse(INPUT_TEST_2, false);
+    // let test_input_part2_simple = parse(INPUT_TEST_PART_2_SIMPLE, true);
     let input = parse(INPUT, false);
-    println!("Part 1   test          {} ", part_1(&test_input));
+    println!("Part 1   test (simple) {} ", part_1(&test_input_2));
+    println!("Part 1   test          {} ", part_1(&test_input_1));
     println!("         validation    {} ", part_1(&input));
-    println!(
-        "Part 2   test          {} ",
-        part_2(&test_input_part2_simple)
-    );
+    // println!(
+    //     "Part 2   test          {} ",
+    //     part_2(&test_input_part2_simple)
+    // );
     // println!("         validation    {} ", part_2(&input));
     println!("Duration: {:?}", start.elapsed());
 }
 
 fn part_1(input: &Input) -> usize {
     let (mut map, mut pos, _size, moves) = input.clone();
+    // print_map(&map, pos, _size);
     for mov in moves {
         step(&mut map, &mut pos, mov);
+        // print_map(&map, pos, _size);
     }
     map.iter()
         .filter_map(|((x, y), c)| {
@@ -41,15 +47,15 @@ fn part_1(input: &Input) -> usize {
         .sum()
 }
 
-fn part_2(input: &Input) -> usize {
-    let (mut map, mut pos, size, moves) = input.clone();
-    // print_map(&map, pos, size);
-    for mov in moves {
-        step(&mut map, &mut pos, mov);
-        // print_map(&map, pos, size);
-    }
-    42
-}
+// fn part_2(input: &Input) -> usize {
+//     let (mut map, mut pos, size, moves) = input.clone();
+//     // print_map(&map, pos, size);
+//     for mov in moves {
+//         step(&mut map, &mut pos, mov);
+//         // print_map(&map, pos, size);
+//     }
+//     42
+// }
 
 fn step(map: &mut Map, pos: &mut Position, mov: Move) {
     let next_pos = (pos.0 + mov.0, pos.1 + mov.1);
@@ -59,44 +65,50 @@ fn step(map: &mut Map, pos: &mut Position, mov: Move) {
             *pos = next_pos;
         }
         'O' => {
-            let mut next_boxes = vec![];
-            let mut i = 1;
+            // 1. Find target (next .) on axis
+            let (mut target_pos, mut offset) = (None, 1);
             loop {
-                let maybe_box_pos = (next_pos.0 + i * mov.0, next_pos.1 + i * mov.1);
-                match map.get(&maybe_box_pos).unwrap() {
-                    '.' => {
-                        next_boxes.push(maybe_box_pos);
+                let maybe_target_pos = (next_pos.0 + offset * mov.0, next_pos.1 + offset * mov.1);
+                let at_target_pos = map.get(&maybe_target_pos).unwrap();
+                if at_target_pos == &'.' {
+                    target_pos = Some(maybe_target_pos);
+                    break;
+                } else if at_target_pos == &'#' {
+                    break;
+                }
+                offset += 1;
+            }
+
+            // 2. List boxes we are trying to move
+            let mut boxes_to_move = vec![];
+            if let Some(target_pos) = target_pos {
+                boxes_to_move.push(next_pos);
+                let mut offset = 1;
+                loop {
+                    let box_pos = (next_pos.0 + offset * mov.0, next_pos.1 + offset * mov.1);
+                    if box_pos == target_pos {
                         break;
                     }
-                    'O' => {
-                        next_boxes.push(maybe_box_pos);
-                        i += 1;
-                    }
-                    '#' => {
-                        next_boxes = vec![];
-                        break;
-                    }
-                    _ => panic!("Oops"),
+                    boxes_to_move.push(box_pos);
+                    offset += 1;
                 }
             }
-            if !next_boxes.is_empty() {
-                next_boxes
+
+            // 3. Check if all boxes can move
+            let boxes_can_move = !boxes_to_move.is_empty()
+                && boxes_to_move
                     .iter()
-                    .map(|next_box_pos| {
-                        let prev_box_pos = (next_box_pos.0 - mov.0, next_box_pos.1 - mov.1);
-                        let prev_char = *map
-                            .get(&(prev_box_pos.0 - mov.0, prev_box_pos.1 - mov.1))
-                            .unwrap();
-                        (next_box_pos, prev_char)
-                    })
-                    .collect::<Vec<_>>()
-                    .iter()
-                    .for_each(|(next_box_pos, prev_char)| {
-                        let prev_box_pos = (next_box_pos.0 - mov.0, next_box_pos.1 - mov.1);
-                        *map.get_mut(next_box_pos).unwrap() = 'O';
-                        let prev = map.get_mut(&prev_box_pos).unwrap();
-                        *prev = *prev_char;
-                    });
+                    .all(|box_pos| map.get(box_pos).unwrap() != &'#');
+
+            // 4. Move if all boxes can move
+            if boxes_can_move {
+                boxes_to_move.iter().for_each(|box_pos| {
+                    *map.get_mut(box_pos).unwrap() = '.';
+                });
+                boxes_to_move.iter().for_each(|box_pos| {
+                    *map.get_mut(&(box_pos.0 + mov.0, box_pos.1 + mov.1))
+                        .unwrap() = 'O';
+                });
                 *pos = next_pos;
             }
         }
