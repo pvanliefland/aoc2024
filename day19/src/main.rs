2fa597db3aa98e68652b2cd4bc22659736ea1296
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-19 10:20:46 +0100

    feat: day19 working

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,30 +1,78 @@
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 const INPUT_TEST: &str = include_str!("../input_test.txt");
-// const INPUT: &str = include_str!("../input.txt");
+const INPUT: &str = include_str!("../input.txt");
 
-type Input = usize;
+type Input = (Vec<String>, Vec<String>);
 
 fn main() {
     let start = Instant::now();
     let test_input = parse(INPUT_TEST);
-    // let input = parse(INPUT);
+    let input = parse(INPUT);
     println!("Part 1   test          {} ", part_1(&test_input));
-    // println!("         validation    {} ", part_1(&input));
-    // println!("Part 2   test          {} ", part_2(&test_input));
-    // println!("         validation    {} ", part_2(&input));
+    println!("         validation    {} ", part_1(&input));
+    println!("Part 2   test          {} ", part_2(&test_input));
+    println!("         validation    {} ", part_2(&input));
     println!("Duration: {:?}", start.elapsed());
 }
 
 fn part_1(input: &Input) -> usize {
-    dbg!(input);
-    42
+    let (available, wanted) = input;
+
+    wanted.iter().filter(|w| is_possible(w, available)).count()
 }
 
-// fn part_2(input: &Input) -> usize {
-//     input.trim().parse::<usize>().unwrap()
-// }
+fn part_2(input: &Input) -> usize {
+    let (available, wanted) = input;
+    let mut cache = HashMap::new();
+
+    wanted
+        .iter()
+        .map(|w| count_possible(w, available, &mut cache))
+        .sum()
+}
+
+fn is_possible(wanted: &str, available: &Vec<String>) -> bool {
+    if wanted.is_empty() {
+        return true;
+    }
+    for towel in available {
+        if towel.len() <= wanted.len()
+            && &wanted[0..towel.len()] == towel
+            && is_possible(&wanted[towel.len()..], available)
+        {
+            return true;
+        }
+    }
+    false
+}
+fn count_possible<'w>(
+    wanted: &'w str,
+    available: &Vec<String>,
+    cache: &mut HashMap<&'w str, usize>,
+) -> usize {
+    if !cache.contains_key(wanted) {
+        let mut count = 0;
+        if wanted.is_empty() {
+            return 1;
+        }
+        for towel in available {
+            if towel.len() <= wanted.len()
+                && &wanted[0..towel.len()] == towel
+                && is_possible(&wanted[towel.len()..], available)
+            {
+                count += count_possible(&wanted[towel.len()..], available, cache);
+            }
+        }
+        cache.insert(wanted, count);
+    }
+    *cache.get(wanted).unwrap()
+}
 
 fn parse(input: &str) -> Input {
-    input.trim().parse().unwrap()
+    let (available, wanted) = input.trim().split_once("\n\n").unwrap();
+    (
+        available.split(", ").map(String::from).collect(),
+        wanted.lines().map(String::from).collect(),
+    )
 }
