c2347ed967c895c7c573af2bc135479a6fd1c876
pvanliefland <pierre.vanliefland@gmail.com>
2024-12-24 07:55:00 +0100

    feat: day24 part 2 wip

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, time::Instant};
 
-const INPUT_TEST: &str = include_str!("../input_test.txt");
+const INPUT_TEST_1: &str = include_str!("../input_test_1.txt");
+const INPUT_TEST_2: &str = include_str!("../input_test_2.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 type Input<'w> = (InitialValues<'w>, Vec<Gate<'w>>);
@@ -9,11 +10,12 @@ type Gate<'g> = (&'g str, &'g str, &'g str, &'g str);
 
 fn main() {
     let start = Instant::now();
-    let test_input = parse(INPUT_TEST);
+    let test_input_1 = parse(INPUT_TEST_1);
+    let test_input_2 = parse(INPUT_TEST_2);
     let input = parse(INPUT);
-    println!("Part 1   test          {} ", part_1(&test_input));
+    println!("Part 1   test          {} ", part_1(&test_input_1));
     println!("         validation    {} ", part_1(&input));
-    // println!("Part 2   test          {} ", part_2(&test_input));
+    println!("Part 2   test          {} ", part_2(&test_input_2));
     // println!("         validation    {} ", part_2(&input));
     println!("Duration: {:?}", start.elapsed());
 }
@@ -33,9 +35,29 @@ fn part_1(input: &Input) -> usize {
         .fold(0, |acc, bit| (acc << 1) + bit as usize)
 }
 
-// fn part_2(input: &Input) -> usize {
-//     input.trim().parse::<usize>().unwrap()
-// }
+fn part_2(input: &Input) -> usize {
+    let (initial_values, _gates) = input;
+    let mut x_wires = initial_values
+        .iter()
+        .filter(|(w, _)| w.starts_with("x"))
+        .collect::<Vec<_>>();
+    x_wires.sort_by_key(|(w, _)| *w);
+    let x = x_wires
+        .into_iter()
+        .rev()
+        .fold(0, |acc, (_, v)| (acc << 1) + *v as usize);
+    let mut y_wires = initial_values
+        .iter()
+        .filter(|(w, _)| w.starts_with("y"))
+        .collect::<Vec<_>>();
+    y_wires.sort_by_key(|(w, _)| *w);
+    let y = y_wires
+        .into_iter()
+        .rev()
+        .fold(0, |acc, (_, v)| (acc << 1) + *v as usize);
+    dbg!(x, y);
+    42
+}
 
 fn compute_gate_value(gate: Gate, all_gates: &Vec<Gate>, initial_values: &InitialValues) -> u8 {
     let (w1, op, w2, _) = gate;
